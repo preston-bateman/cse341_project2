2dad7723be09354b1e5c7ef2b5adcdf1315bf0d6
Preston Bateman <prestonbbateman@gmail.com>
2024-04-01 20:34:40 -0500

    database work

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const mongodb = require('./data/database.js')
 
 const port = process.env.PORT || 3000
 
 app.use('/', require('./routes'))
+
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err)
+    }else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node is running on port ${port}`)
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
