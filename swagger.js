a71a329d23e6220dbba424e9a0ce16904d550a4f
Preston Bateman <prestonbbateman@gmail.com>
2024-04-01 21:32:15 -0500

    swagger addition

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+        title: 'Lakes Api',
+        description: 'Lakes Api'
+    },
+    host: 'localhost:3000',
+    schemes: ['https']
+}
+
+const outputFile = './swagger.json'
+const endpointsFiles = ['./routes/index.js']
+
+//this will generate the swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc)
